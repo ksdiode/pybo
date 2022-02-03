254353882b22cce794b69f05a0d97d6074862947
gusu kim <gusukim@gmail.com>
2022-02-03 13:25:52 +0900

    2장 완성

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
 from .models import Question, Answer
 from django.utils import timezone
-
+from .forms import QuestionForm, AnswerForm
 
 def index(request):
     """
@@ -37,11 +37,38 @@ def answer_create(request, question_id):
     # answer.save()
 
     # 방법2] ForeignKey 관계인경우
-    question.answer_set.create(content = content,
-                  create_date = timezone.now())
+    # question.answer_set.create(content = content,
+    #               create_date = timezone.now())
 
-    return redirect('pybo:detail', question_id=question.id)
+    # return redirect('pybo:detail', question_id=question.id)
 
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/quesiton_detail.html', context)
+
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
 
 
 
